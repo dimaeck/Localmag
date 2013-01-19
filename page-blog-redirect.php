62bb6a4d8d7bcd7c72350e6bab130e7be33ea85f
Michael Frederick <michael.frederickjr@gmail.com>
2013-01-18 19:51:07 -0500

    Restructuring

--- page-blog-redirect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- *
- * Template Name: Blog Redirect
- *
- */
-wp_redirect( 'http://localmag.tumblr.com', 301 ); exit;
-?>
---
+++ wp-content/themes/page-blog-redirect.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ *
+ * Template Name: Blog Redirect
+ *
+ */
+wp_redirect( 'http://localmag.tumblr.com', 301 ); exit;
+?>
